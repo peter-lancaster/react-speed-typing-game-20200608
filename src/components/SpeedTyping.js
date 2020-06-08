ced97922b1500f3c2246340ba33efbb0fb889c3c
peter-lancaster <lancasterp2@hotmail.co.uk>
2020-06-08 10:10:10 +0100

    added wordcount functionality

--- src/components/SpeedTyping.js
+++ src/components/SpeedTyping.js
@@ -5,20 +5,31 @@ function SpeedTyping() {
     const GAME_START_TIME = 4
 
     const [time, setTime] = useState(GAME_START_TIME)
+    const [textInput, setTextInput] = useState("")
     const [wordCount, setWordCount] = useState(0)
 
 
+    function handleChange(event) {
+        const {value} = event.target
+        setTextInput(value)
+    }
 
 
-    
+    function calcWordCount() {
+        // console.log(textInput)
+        // console.log(textInput.trim().split(" ").filter(element => element !== "").length)
+
+        setWordCount(textInput.trim().split(" ").filter(element => element !== "").length)
+    }
+
     useEffect(() => 
-              { if(time > 0) {
-                  setTimeout(() => {setTime(prevTime => prevTime -1)}
-                  ,1000)}
-                else if (time >= 0) {
-                    // calculate number of words
-                }
-
+              {
+                    if(time > 0) {
+                        setTimeout(() => {setTime(prevTime => prevTime -1)}
+                    ,1000)}
+                    else if (time <= 0) {
+                        calcWordCount()
+                    }
 
               }
         ,[time])
@@ -28,10 +39,10 @@ function SpeedTyping() {
     return(
         <main className="speed-typing-main">
             <h1>What is your typing speed?</h1>
-            <textarea></textarea>
+            <textarea onChange={handleChange} value={textInput}></textarea>
             <h2>Number of seconds left : {time} </h2>
-            <button>Start</button>
-            <h2>Your word count was : </h2>
+            <button onClick={calcWordCount}>Start</button>
+            <h2>Your word count was : {wordCount} </h2>
         </main>
     )
 }
