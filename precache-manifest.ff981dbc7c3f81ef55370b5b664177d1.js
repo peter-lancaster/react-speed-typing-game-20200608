1e2fe5ca50b8267efb6d7eede816872f4a02eaab
peter-lancaster <lancasterp2@hotmail.co.uk>
2020-06-11 14:44:37 +0100

    Updates

---
+++ precache-manifest.ff981dbc7c3f81ef55370b5b664177d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f201e72a72a2dcafcd322bf0f9a1e1",
+    "url": "/react-speed-typing-game-20200608/index.html"
+  },
+  {
+    "revision": "ae3f426ab56a39ce5b75",
+    "url": "/react-speed-typing-game-20200608/static/css/main.a04c70e3.chunk.css"
+  },
+  {
+    "revision": "0308bcd632a242a3420f",
+    "url": "/react-speed-typing-game-20200608/static/js/2.e32d29e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-speed-typing-game-20200608/static/js/2.e32d29e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae3f426ab56a39ce5b75",
+    "url": "/react-speed-typing-game-20200608/static/js/main.b426c68a.chunk.js"
+  },
+  {
+    "revision": "0979b0e160c41608323a",
+    "url": "/react-speed-typing-game-20200608/static/js/runtime-main.022ff351.js"
+  }
+]);
